de49b64429baf138d69e85f2173e3d3461fc4f65
sidgupta2106 <sidgupta2106@users.noreply.github.com>
2020-08-03 15:04:24 +0530

    ui changes

--- pclient/src/components/screens/PatientLogin.js
+++ pclient/src/components/screens/PatientLogin.js
@@ -1,40 +1,24 @@
-import React from 'react'
-import {Link,useHistory} from 'react-router-dom'
-import {UserContext} from '../../App'
-import M from 'materialize-css'
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
+import { UserContext } from "../../App";
+import M from "materialize-css";
 
+const PatientLogin = () => {
+  //const ProfilePage = ()=>{
+  //history.push('/Form')
+  //}
 
-
-
-const PatientLogin = ()=>{
-	//const ProfilePage = ()=>{
-	 //history.push('/Form')
-//}
-
-
-
-	return (
-	<div className="mycard">
-          <div className="card auth-card input-field">
-            <h2>Patient Login</h2>
-            <input
-            type="text"
-            placeholder="AADHAR CARD NUMBER "
-            />            
-            	    <h6>
-                <Link to="/Home">Authenticate</Link>
-            </h6>
-            
-    
-        </div>
+  return (
+    <div className="mycard">
+      <div className="card auth-card input-field">
+        <h2>Patient Login</h2>
+        <input type="text" placeholder="AADHAR CARD NUMBER " />
+        <h6>
+          <Link to="/pcomplaint">Authenticate</Link>
+        </h6>
       </div>
+    </div>
+  );
+};
 
-	
-	)
-
-}
-
-
-
-export default PatientLogin
-
+export default PatientLogin;
